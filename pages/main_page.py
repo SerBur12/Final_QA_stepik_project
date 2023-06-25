f748d50da68aab4fdc8df0ccc4567513e67ccd9b
SerBur12 <sergey.burygin@gmail.com>
2023-06-25 14:57:53 +0700

    Add test by assert

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,11 @@
-def main()^
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
 
-pass
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
 
-if __name__ == '__main__':
-    main()
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
